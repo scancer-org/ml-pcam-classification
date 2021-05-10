6973fe1abe7190ea1c38bc15d1bd3e1e22dd042e
Harish Narayanan <hnarayanan@gmail.com>
2021-05-10 14:32:08 +0100

    Add the most up-to-date handler to the repository

--- src/handlers/pcam_classifier_handler.py
+++ src/handlers/pcam_classifier_handler.py
@@ -1,6 +1,8 @@
 import torch
 from torchvision import transforms
 from ts.torch_handler.image_classifier import ImageClassifier
+import numpy as np
+
 
 class PCAMClassifierHandler(ImageClassifier):
     """
@@ -8,16 +10,20 @@ class PCAMClassifierHandler(ImageClassifier):
     default handler. This handler takes an image and returns the the binary prediction indicating presence of metastatic tissue.
     Here method postprocess() has been overridden while others are reused from parent class.
     """
-    image_processing = transforms.Compose([
-      transforms.ToTensor(),
-      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-    ])
+
+    image_processing = transforms.Compose(
+        [transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+    )
+
     def postprocess(self, data):
         """The post process of PCAM Classification converts the predicted output response to a label.
         Args:
             data (list): The predicted output from the Inference with probabilities is passed
             to the post-process function
         Returns:
-            list : A list of dictionary with predictons and explanations are returned.
+            list : A list of with an appropriate prediction.
         """
-        return data.argmax(1).tolist()
+        pred = 1.0 / (1.0 + np.exp(-data.item()))
+        return [int(pred > 0.5)]
+
+        # return data.argmax(1).tolist()
